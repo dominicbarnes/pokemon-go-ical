bd4dfb3292f4fe12e7c5378fddfd6aa7fe5a531b
Dominic Barnes <dominic@dbarnes.info>
2023-11-21 11:12:40 -0800

    many fixes and adding basic auth

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/go-kivik/kivik/v4"
 	_ "github.com/go-kivik/kivik/v4/couchdb"
@@ -15,32 +19,39 @@ import (
 	"app/internal/service"
 )
 
-var couchDSN string
-var couchDB string
+var authUser, authPass string
+var couchDSN, couchDB string
 var debug bool
 var port int
 
 func init() {
+	flag.StringVar(&authUser, "auth-user", "", "The HTTP Basic Auth user to require.")
+	flag.StringVar(&authPass, "auth-pass", "", "The HTTP Basic Auth password to require.")
 	flag.BoolVar(&debug, "debug", false, "Use to turn on debug logging")
 	flag.IntVar(&port, "port", 3000, "The port number to bind to")
 	flag.StringVar(&couchDSN, "couch-dsn", "http://admin:password@localhost:5984", "The CouchDB server")
 	flag.StringVar(&couchDB, "couch-database", "pokemon-go-ical", "The CouchDB database to use.")
+	flag.Parse()
 }
 
 func main() {
+	addr := fmt.Sprintf(":%d", port)
+
 	e := echo.New()
 	e.Debug = debug
 	e.HideBanner = true
 	// TODO: clean up logging
 	e.Use(middleware.Logger())
 
-	client, err := kivik.New("couch", couchDSN)
-	if err != nil {
-		e.Logger.Fatal("failed to initialize couchdb driver", err)
+	if authUser != "" && authPass != "" {
+		e.Use(middleware.BasicAuth(func(user, pass string, c echo.Context) (bool, error) {
+			return user == authUser && pass == authPass, nil
+		}))
 	}
 
-	if err := client.CreateDB(context.TODO(), couchDB); err != nil {
-		e.Logger.Warnf("failed to create db %q: %w", couchDB, err)
+	client, err := initCouchDB(context.TODO())
+	if err != nil {
+		e.Logger.Fatal(err)
 	}
 
 	svc := service.Service{
@@ -48,9 +59,52 @@ func main() {
 		HTTP: http.DefaultClient,
 	}
 
+	e.GET("/", healthCheck)
 	e.POST("/calendars", svc.CalendarCreate)
 	e.PATCH("/calendars/:id", svc.CalendarUpdate)
 	e.GET("/calendars/:id", svc.CalendarGet)
 
-	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", port)))
+	go func() {
+		if err := e.Start(addr); err != nil && err != http.ErrServerClosed {
+			if errors.Is(err, http.ErrServerClosed) {
+				return
+			}
+
+			e.Logger.Fatalf("shutting down the server: %w", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+}
+
+func initCouchDB(ctx context.Context) (*kivik.Client, error) {
+	client, err := kivik.New("couch", couchDSN)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize couchdb driver: %w", err)
+	}
+
+	if ok, err := client.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("failed to ping couchdb: %w", err)
+	} else if !ok {
+		return nil, errors.New("could not connect to database")
+	}
+
+	if ok, err := client.DBExists(ctx, couchDB); err != nil {
+		return nil, fmt.Errorf("failed to check if database exists: %w", err)
+	} else if !ok {
+		return nil, fmt.Errorf("database %q not found", couchDB)
+	}
+
+	return client, nil
+}
+
+func healthCheck(c echo.Context) error {
+	return c.NoContent(http.StatusOK)
 }
