9395d90e1bed5b956b8254fa9f3c920026755570
Dominic Barnes <dbarnes@rippling.com>
2023-12-23 13:07:02 -0700

    avoid generating new commits when events or config are unchanged

--- cmd/generate/main.go
+++ cmd/generate/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"crypto/sha256"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -23,6 +25,7 @@ const defaultSourceURL = "https://raw.githubusercontent.com/bigfoott/ScrapedDuck
 
 type config struct {
 	Input  string `flag:"-i,--input" help:"The input configuration file" default:"config.yaml"`
+	Cache  string `flag:"-c,--cache" help:"The cache location" default:"cache.yaml"`
 	Output string `flag:"-o,--output" help:"The output directory" default:"./data"`
 }
 
@@ -30,6 +33,11 @@ type calendar struct {
 	Timezone string   `yaml:"timezone"`
 	Include  []string `yaml:"include"`
 	Exclude  []string `yaml:"exclude"`
+}
+
+type cache struct {
+	Config string `yaml:"config"`
+	Events string `yaml:"events"`
 }
 
 func main() {
@@ -41,17 +49,27 @@ func main() {
 			return fmt.Errorf("failed to create output directory: %w", err)
 		}
 
-		calendars, err := getCalendars(ctx, config.Input)
+		previous, err := readCache(ctx, config.Cache)
+		if err != nil {
+			return fmt.Errorf("failed to get cache: %w", err)
+		}
+
+		configHash, calendars, err := getCalendars(ctx, config.Input)
 		if err != nil {
 			return fmt.Errorf("failed to get calendars: %w", err)
 		}
 		slog.Info("found calendars", slog.Int("calendar_count", len(calendars)))
 
-		events, err := getEvents(ctx, config.Input)
+		eventsHash, events, err := getEvents(ctx, config.Input)
 		if err != nil {
 			return fmt.Errorf("failed to get events: %w", err)
 		}
 		slog.Info("found events", slog.Int("event_count", len(events)))
+
+		if previous.Config == configHash && previous.Events == eventsHash {
+			slog.Info("no changes detected in calendar config or events")
+			return nil
+		}
 
 		for name, calendar := range calendars {
 			logger := slog.With(slog.String("name", name))
@@ -87,17 +105,57 @@ func main() {
 			logger.Info("output calendar", slog.String("path", output))
 		}
 
+		if err := writeCache(ctx, config.Cache, configHash, eventsHash); err != nil {
+			return fmt.Errorf("failed to write cache: %w", err)
+		}
+
 		slog.Info("done")
 		return nil
 	}))
 }
 
-func getEvents(ctx context.Context, input string) ([]ical.LeekDuckEvent, error) {
+func readCache(ctx context.Context, path string) (*cache, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return new(cache), nil
+		}
+
+		return nil, err
+	}
+
+	data, err := io.ReadAll(f)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read cache: %w", err)
+	}
+
+	var output cache
+	if err := yaml.Unmarshal(data, &output); err != nil {
+		return nil, fmt.Errorf("failed to decode cache: %w", err)
+	}
+
+	return &output, nil
+}
+
+func getEvents(ctx context.Context, input string) (string, []ical.LeekDuckEvent, error) {
 	res, err := http.Get(defaultSourceURL)
 	if err != nil {
-		return nil, fmt.Errorf("failed to download events: %w", err)
+		return "", nil, fmt.Errorf("failed to download events: %w", err)
 	}
-	return decodeEvents(ctx, res.Body)
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", nil, fmt.Errorf("failed to read events: %w", err)
+	}
+
+	events, err := decodeEvents(ctx, bytes.NewBuffer(data))
+	if err != nil {
+		return "", nil, fmt.Errorf("failed to decode events: %w", err)
+	}
+
+	hash := fmt.Sprintf("%x", sha256.Sum256(data))
+	return hash, events, nil
 }
 
 func decodeEvents(ctx context.Context, data io.Reader) ([]ical.LeekDuckEvent, error) {
@@ -117,29 +175,23 @@ func getTimezone(ctx context.Context, input string) (*time.Location, error) {
 	return tz, nil
 }
 
-func getCalendars(ctx context.Context, config string) (map[string]calendar, error) {
+func getCalendars(ctx context.Context, config string) (string, map[string]calendar, error) {
 	data, err := os.ReadFile(config)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read config file: %w", err)
+		return "", nil, fmt.Errorf("failed to read config file: %w", err)
 	}
 
 	var output map[string]calendar
 	if err := yaml.Unmarshal(data, &output); err != nil {
-		return nil, fmt.Errorf("failed to decode config as YAML: %w", err)
+		return "", nil, fmt.Errorf("failed to decode config as YAML: %w", err)
 	}
-	return output, nil
+
+	hash := fmt.Sprintf("%x", sha256.Sum256(data))
+	return hash, output, nil
 }
 
 func writeCalendar(ctx context.Context, cal *ics.Calendar, output string) error {
 	ics := cal.Serialize()
-
-	if output == "" {
-		if _, err := fmt.Fprint(os.Stdout, ics); err != nil {
-			return fmt.Errorf("could not write calendar: %w", err)
-		}
-		return nil
-	}
-
 	f, err := os.OpenFile(output, os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return fmt.Errorf("could not open file: %w", err)
@@ -148,4 +200,17 @@ func writeCalendar(ctx context.Context, cal *ics.Calendar, output string) error 
 		return fmt.Errorf("could not write calendar: %w", err)
 	}
 	return nil
+}
+
+func writeCache(ctx context.Context, path, config, events string) error {
+	output := cache{
+		Config: config,
+		Events: events,
+	}
+	data, err := yaml.Marshal(output)
+	if err != nil {
+		return fmt.Errorf("failed to encode cache: %w", err)
+	}
+
+	return os.WriteFile(path, data, 0644)
 }
